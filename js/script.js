e709fbee1861953ab80127f7064ec941ec6f7bb9
SanviDev <smcaldabe@gmail.com>
2025-07-11 11:43:00 -0300

    finish. new function: reset

--- js/script.js
+++ js/script.js
@@ -3,13 +3,44 @@ const proteinasRestantes = document.getElementById("proteinasRestantes");
 const grasasRestantes = document.getElementById("grasasRestantes");
 const carbohidratosRestantes = document.getElementById("carbohidratosRestantes");
 const contenedor = document.getElementById("container");
+const resetButton = document.getElementById("resetButton");
+
+
+
+// Guardar valores en LocalStorage
+if (!localStorage.getItem("proteinas")) {
+    localStorage.setItem("proteinas", 140);
+}
+
+if (!localStorage.getItem("grasas")) {
+    localStorage.setItem("grasas", 70);
+}
+
+if (!localStorage.getItem("carbohidratos")) {
+    localStorage.setItem("carbohidratos", 353);
+}
+
+let proteinas = parseFloat(localStorage.getItem("proteinas"));
+let grasas = parseFloat(localStorage.getItem("grasas"));
+let carbohidratos = parseFloat(localStorage.getItem("carbohidratos"));
+// Actualizar valores en LocalStorage
+function actualizarValores() {
+    localStorage.setItem("proteinas", proteinas);
+    localStorage.setItem("grasas", grasas);
+    localStorage.setItem("carbohidratos", carbohidratos);
+    proteinasRestantes.textContent = proteinas.toFixed(2);
+    grasasRestantes.textContent = grasas.toFixed(2);
+    carbohidratosRestantes.textContent = carbohidratos.toFixed(2);
+}
+
+actualizarValores();
 
 fetch(jsonPath)
     .then(response => response.json())
     .then(alimentos => {
         alimentos.alimentos.forEach(a => {
-            contenedor.innerHTML += 
-            `<div class="alimento" id="${a.nombre}">
+            contenedor.innerHTML +=
+                `<div class="alimento" id="${a.nombre}">
                 <p class="nombre-alimento">${a.nombre}</p>
                 <div class="cantidad">
                     <p>Cantidad</p>
@@ -19,24 +50,41 @@ fetch(jsonPath)
                     </div>
                 </div>
             </div>`;
-            
+
         });
         document.querySelectorAll(".confirmar").forEach(btn => {
-                btn.addEventListener("click", ()=>{
-                    const nombre = btn.getAttribute("attribute-name");
-                    const alimento = alimentos.alimentos.find(a = a.nombre === nombre);
-                    const input = btn.parentElement.querySelector('input');
-                    const cantidad = parseFloat(input.value)
+            btn.addEventListener("click", () => {
+                const nombre = btn.getAttribute("attribute-name");
+                const alimento = alimentos.alimentos.find(a => a.nombre === nombre);
+                const input = btn.parentElement.querySelector('input');
+                const cantidad = parseFloat(input.value)
 
-                    if (!cantidad || cantidad <= 0) return;
+                if (!cantidad || cantidad <= 0) return;
 
-                    //* Agregar if para tratar por separado los alimentos que se cuentan como unidad
-                    //* De los que se cuentan por gramo. Por unidad multiplicar cantidad por valores
-                    //* De los que se cuentan por gramo allar factor (cantidad/alimento.cantidad_gramos)
-                    //* Y restar a las proteinas restantes el producto de alimento.proteinas * factor
-
-
-                })
+                if (nombre.toLowerCase() == "huevo"
+                    || nombre.toLowerCase() == "banana"
+                    || nombre.toLowerCase() == "manzana"
+                    || nombre.toLowerCase() == "pera"
+                    || nombre.toLowerCase() == "naranja"
+                ) {
+                    proteinas -= (alimento.proteina * cantidad);
+                    grasas -= (alimento.grasas * cantidad);
+                    carbohidratos -= (alimento.carbohidratos * cantidad);
+                    actualizarValores();
+                } else {
+                    const factor = cantidad / alimento.cantidad_gramos
+                    proteinas -= (alimento.proteina * factor);
+                    grasas -= (alimento.grasas * factor);
+                    carbohidratos -= (alimento.carbohidratos * factor);
+                    actualizarValores();
+                }
             })
+        })
     });
 
+resetButton.addEventListener("click", () => {
+    proteinas = 140;
+    grasas = 70;
+    carbohidratos = 353;
+    actualizarValores();
+})
